fe0e9c154e21665b6f6a4f72c831ff69069317a3
Lua Network <106291312+luanet@users.noreply.github.com>
2022-08-15 14:53:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 
 const fs = require('fs')
 const path = require('path')
+const log = require('electron-log')
 
 module.exports.path = function () {
   const paths = [
+    path.resolve(path.join(__dirname, '..', 'node_modules', 'go-ipfs', 'go-ipfs', 'ipfs')),
+    path.resolve(path.join(__dirname, '..', 'node_modules', 'go-ipfs', 'go-ipfs', 'ipfs.exe')),
     path.resolve(path.join(__dirname, '..', 'go-ipfs', 'ipfs')),
     path.resolve(path.join(__dirname, '..', 'go-ipfs', 'ipfs.exe'))
   ]
 
+  log.info(paths)
   for (const bin of paths) {
-    if (fs.existsSync(bin)) {
-      return bin
+    const path = bin.replace('app.asar', 'app.asar.unpacked')
+    if (fs.existsSync(path)) {
+      return path
     }
   }
 
