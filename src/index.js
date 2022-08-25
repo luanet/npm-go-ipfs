7d4382d3d4095ee28ca3c8b7dcebc8726cef10f8
Lua Network <106291312+luanet@users.noreply.github.com>
2022-08-25 20:29:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const log = require('electron-log')
 const download = require('./download')
 
 module.exports.path = function () {
+  log.info(__dirname);
   const paths = [
-    path.resolve(path.join(__dirname, '..', 'node_modules', 'go-ipfs', 'go-ipfs', 'ipfs')),
-    path.resolve(path.join(__dirname, '..', 'node_modules', 'go-ipfs', 'go-ipfs', 'ipfs.exe')),
-    path.resolve(path.join(__dirname, '..', 'go-ipfs', 'ipfs')),
-    path.resolve(path.join(__dirname, '..', 'go-ipfs', 'ipfs.exe'))
+    // packed
+    path.resolve(path.join(__dirname, '..', '..', 'bin', 'go-ipfs', 'go-ipfs', 'ipfs')),
+    path.resolve(path.join(__dirname, '..', '..', 'bin', 'go-ipfs', 'go-ipfs', 'ipfs.exe')),
+    // unpacked
+    path.resolve(path.join(__dirname, '..', 'go-ipfs', 'go-ipfs', 'ipfs')),
+    path.resolve(path.join(__dirname, '..', 'go-ipfs', 'go-ipfs', 'ipfs.exe')),
   ]
 
   for (const bin of paths) {
